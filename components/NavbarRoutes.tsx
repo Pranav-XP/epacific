6a99542ecac3e0b9d8b9f848b50753ba63cd5d3c
Pranav <112617967+Pranav-XP@users.noreply.github.com>
2023-09-20 16:49:28 +1200

    Navbar updates

--- components/NavbarRoutes.tsx
+++ components/NavbarRoutes.tsx
@@ -3,16 +3,31 @@
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
 import { usePathname, useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 const NavbarRoutes = () => {
   const pathname = usePathname();
   const router = useRouter();
   const isTeacherPage = pathname?.startsWith("/teacher");
-  const isPlayer = pathname?.includes("/chapter");
+  const isPlayerPage = pathname?.includes("/chapter");
 
   return (
     <div className="flex gap-x-2 ml-auto">
-      <UserButton></UserButton>
+      {isTeacherPage || isPlayerPage ? (
+        <Button>
+          <LogOut className="h-4 w-4 mr-2" />
+          Exit
+        </Button>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button className="text-slate-700" size="sm" variant={"ghost"}>
+            Teacher Mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/"></UserButton>
     </div>
   );
 };
